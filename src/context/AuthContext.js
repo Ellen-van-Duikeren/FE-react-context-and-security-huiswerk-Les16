13d01291d5f37cd7860e2b88d648ac1b50351c11
Ellen van Duikeren <e.vanduikeren@gmail.com>
2022-12-21 14:27:44 +0100

    Debugging bonusexercise

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,26 +4,27 @@ import {useNavigate} from "react-router-dom";
 export const AuthContext = React.createContext({});
 
 function AuthContextProvider({children}) {
-    const [isAuth, setIsAuth] = useState({
+    const [auth, setAuth] = useState({
         isAuth: false,
         user: ""
     });
     const navigate = useNavigate();
 
     function login(email) {
-        setIsAuth({isAuth: true, user: email});
+        setAuth({isAuth: true, user: email});
         console.log("Gebruiker is ingelogd met het emailadres: " + email + "!");
         navigate('/profile');
     }
 
     function logout() {
-        setIsAuth(false);
+        setAuth({isAuth: false, user: ""});
         console.log("Gebruiker is uitgelogd!");
         navigate('/');
     }
 
     const data = {
-        isAuth: isAuth,
+        isAuth: auth.isAuth,
+        user: auth.user,
         login: login,
         logout: logout
     }
